765e738caa5cfe6693bf120ce19bd3479e0497df
Alamgir kabir <alamgir.saguli@gmail.com>
2022-09-30 12:12:29 +0600

    first commit

---
+++ js/calcul.js
@@ -0,0 +1,21 @@
+var val=document.getElementById("inp");
+
+            function AT_add(v){
+            val.value+=v;
+            }
+                       
+            function cls(){
+            val.value="";
+            }
+            
+            function exe(){
+            val.value=eval(val.value);
+            }
+            
+            function cancel(){
+            val.value=val.value.substr(0,val.value.length-1);
+            }
+
+function myFunction(){
+    document.getElementById('btn').innerHTML='hellow world'
+}
